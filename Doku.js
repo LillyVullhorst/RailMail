d63ec74369b36d01fd94f68696ee1ef98e14d042
Sylvia Rinke <Sylvia.Rinke@stud.h-da.de>
2019-01-17 01:40:04 +0100

    buttons

--- Doku.js
+++ Doku.js
@@ -1,14 +1,15 @@
 /* jslint esversion: 6  */
 
 //Hintergründe
-let hintergrund = loadImage("FahrplanDoku1.png");
-let hintergrundMob = loadImage("FahrplanDokuMobilität.png");
-let hintergrundtech = loadImage("FahrplanDokuTechnik.png");
+let hintergrund = loadImage("Hintergrund/FahrplanDoku1.png");
+let hintergrundMob = loadImage("Hintergrund/FahrplanDokuMobilität.png");
+let hintergrundtech = loadImage("Hintergrund/FahrplanDokuTechnik.png");
+let hintergrundKunde =loadImage("Hintergrund/FahrplanDokuKunden.png");
 
 let buttonkasten = loadImage("ButtonHilfe.png");
 
 //Texte
-let text1 = loadImage("Einleitung.png");
+let text1 = loadImage("Texte/Einleitung.png");
 
 let count = 0;
 
@@ -26,36 +27,56 @@ class Background{
         image(this.pic,this.X,this.Y,this.myWidth,this.myHeight);
     }   
 }
-
+//Hintergrund
 let back1 = new Background(hintergrund, 0,0,windowWidth,windowWidth*2);
 let backMob = new Background(hintergrundMob, 0,0,windowWidth,windowWidth*2);
-let backtech = new Background(hintergrundtech, 0,0,windowWidth,windowWidth*2);
+let backtech = new Background(hintergrundtech, 0,0,windowWidth,windowWidth*3.25);
+let backKunde = new Background(hintergrundKunde,0,0,windowWidth,windowWidth*2);
 
-let bkasten = new Background(buttonkasten, mouseX>windowWidth/3)&&(mouseX<windowWidth/3+windowWidth/15)&&(mouseY>windowWidth/4)&&(mouseY<windowWidth/4+windowWidth/15);
+let bkasten = new Background(buttonkasten,windowWidth/1.1,windowWidth/1,windowWidth/15,windowWidth/15);
 
 //Texte
 let einleitung = new Background(text1,windowWidth/9,windowWidth/13,430,300);
 
 //Button-Rechnungen
-function mouseClicked(){                //x=je größer je näher ist die links , y = je größer je weiter ist die oben
+//x=je größer je näher ist die links , y = je größer je weiter ist die oben
+function mouseClicked(){    
+    //->Mobilität für uns            
     if ((count==0)&&(mouseX>windowWidth/1.17)&&(mouseX<windowWidth/1.17+windowWidth/15)&&(mouseY>windowWidth/6.15)&&(mouseY<windowWidth/6.15+windowWidth/15)){
         count =2; 
     }
-    if ((count==0)&&(mouseX>windowWidth/3)&&(mouseX<windowWidth/3+windowWidth/15)&&(mouseY>windowWidth/2)&&(mouseY<windowWidth/2+windowWidth/15)){
+    //Der Hauptumschagspunkt
+    if ((count==0)&&(mouseX>windowWidth/20)&&(mouseX<windowWidth/20+windowWidth/15)&&(mouseY>windowWidth/0.6)&&(mouseY<windowWidth/0.6+windowWidth/15)){
         count =3; 
+    }
+    //Nachricht
+    if ((count==0)&&(mouseX>windowWidth/6)&&(mouseX<windowWidth/6+windowWidth/15)&&(mouseY>windowWidth/0.53)&&(mouseY<windowWidth/0.53+windowWidth/15)){
+        count =4; 
+    }
+    //Unser Kunde
+    if ((count==0)&&(mouseX>windowWidth/1.28)&&(mouseX<windowWidth/1.28+windowWidth/15)&&(mouseY>windowWidth/1.14)&&(mouseY<windowWidth/1.14+windowWidth/15)){
+        count =5; 
+    }
+    //Unser Endnutzer
+    if ((count==0)&&(mouseX>windowWidth/1.1)&&(mouseX<windowWidth/1.1+windowWidth/15)&&(mouseY>windowWidth/1)&&(mouseY<windowWidth/1+windowWidth/15)){
+        count =5; 
     }
 }
 
 function draw(){
+    clear();
     if (count==0){
     back1.display();
     einleitung.display();
-    bkasten.display();
+    //bkasten.display();
     }
     if (count==2){
     backMob.display();
     }
     if (count==3){
         backtech.display();
+    }
+    if (count==5){
+        backKunde.display();
     }
     }
--- p5setup.js
+++ p5setup.js
@@ -1,6 +1,12 @@
 new p5();
 function setup() {
+    //Require einfügen?
+   /* if (count==3){ createCanvas(windowWidth,windowWidth*3.25);  
+   } */
+   
     createCanvas(windowWidth,windowWidth*2);
+    
+
     frameRate(30);
 }
 
