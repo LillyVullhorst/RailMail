592fca490231c46e9bdf61aaa1271e50d29069e6
Sylvia Rinke <Sylvia.Rinke@stud.h-da.de>
2019-01-29 11:18:09 +0100

    new

--- Doku.js
+++ Doku.js
@@ -30,6 +30,7 @@ let text11 = loadImage("Texte/techStation.png");
 
 let text12 = loadImage("Texte/derKunde.png");
 let text13 = loadImage("Texte/derEndkunde.png");
+let text14 = loadImage("Texte/Standort.png");
 
 
 //let gif = loadImage("Schienen.gif");
@@ -51,6 +52,9 @@ let belt = loadImage("Bilder/Fließbandsegmente.png");
 let waggon = loadImage("Bilder/Wagonaußen.png");
 let innerWaggon = loadImage("Bilder/obereWagenEbene.png");
 let stationEbene = loadImage("Bilder/Paketstationoben.png");
+
+let map = loadImage("Bilder/Abdeckungskarte.png");
+
 
 
 
@@ -95,7 +99,7 @@ class Background{
 let back1 = new Background(hintergrund, 0,0,windowWidth,windowWidth*2);
 let backMob = new Background(hintergrundMob, 0,0,windowWidth,windowWidth*2);
 let backtech = new Background(hintergrundtech, 0,0,windowWidth,windowWidth*3.25);
-let backKunde = new Background(hintergrundKunde,0,0,windowWidth,windowWidth*3.25);
+let backKunde = new Background(hintergrundKunde,0,0,windowWidth,windowWidth*3.76);
 let abholung = new Background (hintergrundAbholung, 0,0,windowWidth,windowWidth*1.5);
 let topNav = new Background(topBar,0,0,windowWidth,windowWidth/21.5);
 let manNav = new Background(topMan,0,windowWidth/manY,windowWidth,windowWidth/21.5);
@@ -106,25 +110,26 @@ let shadowNav = new Background(topShadow,0,windowWidth/16.5,windowWidth,windowWi
 let bkasten = new Background(buttonkasten,windowWidth/1.13,windowWidth/0.46,windowWidth/15,windowWidth/15);
 
 //Texte
-let einleitung = new Background(text1,windowWidth/9,windowWidth/8,windowWidth/4.465,windowWidth/6.4);
+let einleitung = new Background(text1,windowWidth/2.6,windowWidth/3.48,windowWidth/4.465,windowWidth/6.4);
 let problem = new Background(text2,windowWidth/9,windowWidth/1.4,windowWidth/2.954,windowWidth/6.4);
 let einPaketBestellen = new Background(text3,windowWidth/4,windowWidth/0.8,windowWidth/2.06,windowWidth/2.4);
 let Nachricht = new Background(text4,windowWidth/4.98,windowWidth/6,windowWidth/2.612,windowWidth/2.07);
 let DieStation = new Background(text5,windowWidth/2.5,windowWidth/1.283,windowWidth/2.99,windowWidth/3.76);
 let LastMile = new Background(text6,windowWidth/7,windowWidth/0.78,windowWidth/1.692,windowWidth/9.746);
 let mobil = new Background(text7,windowWidth/3.2,windowWidth/4,windowWidth/2.4,windowWidth/3.24);
-let abstract = new Background(text8,windowWidth/3.2,windowWidth/1.4,windowWidth/2.49,windowWidth/6.9);
+let abstract = new Background(text8,windowWidth/3.2,windowWidth/1.32,windowWidth/2.49,windowWidth/6.9);
 
 let umschlagspunkt = new Background(text9,windowWidth/3.4,windowWidth/5,windowWidth/2.93,windowWidth/3.1);
-let wagonText = new Background (text10,windowWidth/4,windowWidth/0.82,windowWidth/2.9,windowWidth/2.43);
+let wagonText = new Background (text10,windowWidth/4,windowWidth/0.78,windowWidth/2.9,windowWidth/2.43);
 let innerStation = new Background(text11,windowWidth/3,windowWidth/0.43,windowWidth/2.9,windowWidth/3.3);
 
-let customer = new Background(text12,windowWidth/5.2,windowWidth/4,windowWidth/1.4,windowWidth/2.75);
-let endcustomer = new Background(text13,windowWidth/5.2,windowWidth/1.45,windowWidth/2.94,windowWidth/3.51);
+let customer = new Background(text12,windowWidth/5.2,windowWidth/3.5,windowWidth/1.4,windowWidth/2.75);
+let endcustomer = new Background(text13,windowWidth/5.2,windowWidth/1.27,windowWidth/2.94,windowWidth/3.51);
+let ort = new Background(text14,windowWidth/5.2,windowWidth/0.76,windowWidth/1.45,windowWidth/9.8);
 
 //Bilder
 let umschlagslagerbild = new Background(umschlagslager, windowWidth/3.4,windowWidth/1.8,windowWidth/2.86,windowWidth/4.8);
-let dasTeam = new Background(team,windowWidth/9,windowWidth/3,windowWidth/4.27,windowWidth/6.4);
+let dasTeam = new Background(team,windowWidth/9,windowWidth/3.5,windowWidth/4.27,windowWidth/6.4);
 let stationAus = new Background(station1,windowWidth/7,windowWidth/1.2,windowWidth/4.8,windowWidth/4.8);
 let stationAn = new Background(station2,windowWidth/7,windowWidth/1.2,windowWidth/4.8,windowWidth/4.8);
 let stationFach = new Background(station3,windowWidth/7,windowWidth/1.2,windowWidth/4.8,windowWidth/4.8);
@@ -134,13 +139,16 @@ let dasRadHover = new Background(radhover,windowWidth/2.48,windowWidth/0.85,wind
 let Tasche = new Background(bag,windowWidth/4,windowWidth/0.9,windowWidth/10.67,windowWidth/6.4);
 let Taschehover = new Background (baghover,windowWidth/4,windowWidth/0.9,windowWidth/10.10,windowWidth/6.4);
 
-let derZug = new Background(train,windowWidth/15.5,windowWidth/15,windowWidth/30.5,windowWidth/4.03);
+//var derZug = new Background(train,windowWidth/15.5,windowWidth/15,windowWidth/30.5,windowWidth/4.03);
 let Fließband = new Background (belt,windowWidth/1.5,windowWidth/3.8,windowWidth/3.8,windowWidth/3.75);
-let derWagon = new Background(waggon,windowWidth/3.2,windowWidth/0.98,windowWidth/2.3,windowWidth/3.3);
-let wagonEbene = new Background(innerWaggon,windowWidth/5.2,windowWidth/0.6,windowWidth/2.3,windowWidth/4.26);
+let derWagon = new Background(waggon,windowWidth/3.2,windowWidth/0.96,windowWidth/2.3,windowWidth/3.3);
+let wagonEbene = new Background(innerWaggon,windowWidth/5.2,windowWidth/0.57,windowWidth/2.3,windowWidth/4.26);
 let inStation = new Background(stationEbene,windowWidth/3,windowWidth/0.38,windowWidth/1.86,windowWidth/3.575);
+
+let abdeckung = new Background(map,windowWidth/5.2,windowWidth/0.7,windowWidth/2.03,windowWidth/2.46);
 //let Schienen = new Background(gif,windowWidth/9,windowWidth/1,500,250)
 
+//var derZug = document.getElementById("Z"); 
 //Button-Rechnungen
 //x=je größer je näher ist die links , y = je größer je weiter ist die oben
 function mouseClicked(){    
@@ -174,6 +182,7 @@ function mouseClicked(){
     //Unsere Kunden
     if ((count==0)&&(mouseX>windowWidth/1.1)&&(mouseX<windowWidth/1.1+windowWidth/15)&&(mouseY>windowWidth/1)&&(mouseY<windowWidth/1+windowWidth/15)){
         count =5; 
+        createCanvas(windowWidth,windowWidth*3.76); 
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
     }
@@ -220,12 +229,16 @@ function mouseClicked(){
         pack=1;
     }
 }
+jQuery(document).ready(function(){
+var zOffset= jQuery("nav").offset().top;
+alert(zOffset);
+});
 
-     
 
 
 function draw(){
     clear();
+   
     
     if((mouseX>0)&&(mouseX<windowWidth)&&(mouseY>windowWidth/350)&&(mouseY<windowWidth/100)){
         manY=manY-100;
@@ -255,6 +268,9 @@ function draw(){
         umschlagslagerbild.display(); 
         umschlagspunkt.display();
         derZug.display();
+        
+    
+       
         Fließband.display();
         derWagon.display();
         wagonText.display();
@@ -323,6 +339,8 @@ function draw(){
         backKunde.display(); 
         customer.display();
         endcustomer.display();
+        abdeckung.display();
+        ort.display();
     }
     console.log(manY);
      //Navigation
@@ -331,6 +349,7 @@ function draw(){
     techNav.display();
     manNav.display();
     topNav.display();
-  
+
 
     }
+    
